501be33f921c77809563e9b0d88387d7807c9235
CD7567 <alexander.y.koshelev@gmail.com>
2024-02-06 17:29:32 +0300

    Fixed list stack

--- task_1/src/stack-lib/include/stack-lib/list_stack.hxx
+++ task_1/src/stack-lib/include/stack-lib/list_stack.hxx
@@ -12,7 +12,7 @@ namespace stack_lib {
     template <typename T>
     struct Node {
         T elem_;
-        Node* prev_;
+        Node<T>* prev_;
     };
 
     template <typename T>
@@ -28,7 +28,7 @@ namespace stack_lib {
 
         ListStack() = default;
 
-        ListStack(const ListStack& other) {
+        ListStack(const ListStack& other) : size_(other.size_) {
             if (other.top_ != nullptr) {
                 Node<T>* other_it = other.top_;
                 Node<T>* this_it = nullptr;
@@ -36,14 +36,14 @@ namespace stack_lib {
                 this_it = top_ = new Node<T>{other.top_->elem_, nullptr};
 
                 while (other_it->prev_ != nullptr) {
-                    this_it->prev = new Node<T>{other.top_->prev_->elem_, nullptr};
+                    this_it->prev_ = new Node<T>{other.top_->prev_->elem_, nullptr};
                     other_it = other_it->prev_;
                     this_it = this_it->prev_;
                 }
             }
         }
 
-        ListStack(ListStack&& other) noexcept : top_(other.top_), size_(other.size_){
+        ListStack(ListStack&& other) noexcept : top_(other.top_), size_(other.size_) {
             other.size_ = 0;
             other.top_ = nullptr;
         }
@@ -76,11 +76,22 @@ namespace stack_lib {
                     this_it = this_it->prev_;
                 }
             }
+            
+            size_ = other.size_;
 
             return *this;
         }
 
         auto& operator=(ListStack<T>&& other) noexcept {
+            Node<T>* tmp = nullptr;
+
+            while (top_ != nullptr) {
+                tmp = top_;
+                top_ = top_->prev_;
+            
+                delete tmp;
+            }
+
             size_ = other.size_;
             top_ = other.top_;
             other.size_ = 0;
