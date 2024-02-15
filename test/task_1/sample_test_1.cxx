0feb161f32923cd6eb75f6c32ff0da1dc500f64b
CD7567 <alexander.y.koshelev@gmail.com>
2024-02-15 11:40:47 +0300

    Migration to doctest

---
+++ test/task_1/sample_test_1.cxx
@@ -0,0 +1,9 @@
+//
+// Created by cd7567 on 2/15/24.
+//
+
+#include "../doctest/doctest.h"
+
+TEST_CASE("TEST_1") {
+    CHECK(1 == 1);
+}
---
+++ test/test_runner.cxx
@@ -0,0 +1,10 @@
+//
+// Created by cd7567 on 2/15/24.
+//
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest/doctest.h"
+
+#include "task_1/sample_test_1.cxx"
+#include "task_1/sample_test_2.cxx"
