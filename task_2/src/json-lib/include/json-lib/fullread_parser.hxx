9464f148b86de78913e6f2e0ad2917c3c7d178d1
CD7567 <alexander.y.koshelev@gmail.com>
2024-03-14 12:13:35 +0300

    Add FullreadParser

---
+++ task_2/src/json-lib/include/json-lib/fullread_parser.hxx
@@ -0,0 +1,39 @@
+#ifndef JSON_LIB_BUFFERED_PARSER_HXX
+#define JSON_LIB_BUFFERED_PARSER_HXX
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "json-lib//common.hxx"
+
+namespace json_lib {
+
+class FullreadParser {
+  private:
+    std::ifstream f_in_stream_;
+    std::vector<JSONBlock> blocks_;
+    std::string buffer_;
+    std::string::iterator buffer_it_;
+
+    inline void ParseUserEntry(JSONEntry<std::string>& entry);
+
+    inline void ParseQuestionEntry(JSONEntry<std::string>& entry);
+
+    inline void ParseScoreEntry(JSONEntry<int>& entry);
+
+    inline void ParseBlock(JSONBlock& block);
+
+  public:
+    explicit FullreadParser(const char* json_path) : f_in_stream_(json_path) {
+        std::getline(f_in_stream_, buffer_);
+    }
+
+    void Parse();
+
+    std::vector<JSONBlock>& GetData();
+};
+
+}  // namespace json_lib
+
+#endif  // JSON_LIB_BUFFERED_PARSER_HXX
