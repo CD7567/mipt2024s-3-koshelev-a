88daa7db1f02ea1f87a6363426ea65e09148cbec
CD7567 <alexander.y.koshelev@gmail.com>
2024-03-20 12:04:38 +0300

    Add test data copying

--- test/common_definitions.hxx
+++ test/common_definitions.hxx
@@ -1,7 +1,7 @@
 #ifndef TEST_COMMON_DEFINITIONS_HXX
 #define TEST_COMMON_DEFINITIONS_HXX
 
-constexpr const char* TEST_JSON_PATH = "./data-test.json";
+constexpr const char* TEST_JSON_PATH = "data/data-parser-test.json";
 
 struct TestStruct {
     size_t number_ = 1000L;
