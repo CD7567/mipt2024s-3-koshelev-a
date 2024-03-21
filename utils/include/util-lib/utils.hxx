d168cbb3766dd569593e12e6e389d9414693e43e
CD7567 <alexander.y.koshelev@gmail.com>
2024-03-21 04:24:52 +0300

    Add timeit function

--- utils/include/util-lib/utils.hxx
+++ utils/include/util-lib/utils.hxx
@@ -1,6 +1,7 @@
 #ifndef UTILS_LIB_UTILS_HXX
 #define UTILS_LIB_UTILS_HXX
 
+#include <chrono>
 #include <cstddef>
 #include <string>
 
@@ -16,10 +17,27 @@ struct TestStruct {
  * @param args Format args
  * @return Formatted string
  */
-template<typename... Args>
-std::string dyn_format(std::string_view format_str, Args&&... args)
-{
+template <typename... Args>
+std::string dyn_format(std::string_view format_str, Args&&... args) {
     return std::vformat(format_str, std::make_format_args(args...));
+}
+
+/**
+ * Time profile given function wrapped in lambda
+ * @tparam ChronoT std::chrono result unit type
+ * @tparam Lambda Profiled function type
+ * @tparam Args Profiled function arguments types
+ * @param lambda Profiled function
+ * @param args Profiled function arguments
+ * @return Time in given units
+ */
+template <typename ChronoT, typename Lambda, typename... Args>
+inline size_t timeit(Lambda&& lambda, Args&&... args) {
+    auto start = std::chrono::high_resolution_clock::now();
+    lambda(args...);
+    auto end = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration_cast<ChronoT>(end - start).count();
 }
 
 #endif  // UTILS_LIB_UTILS_HXX
