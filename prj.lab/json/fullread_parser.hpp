30c15a7a8724f23b230167c08f9f877dc255f5dc
CD7567 <alexander.y.koshelev@gmail.com>
2024-04-03 10:57:39 +0300

    Move json parsers

---
+++ prj.lab/json/fullread_parser.hpp
@@ -0,0 +1,35 @@
+#ifndef JSON_LIB_BUFFERED_PARSER_HXX
+#define JSON_LIB_BUFFERED_PARSER_HXX
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "common.hpp"
+
+class FullreadParser {
+  private:
+    std::ifstream f_in_stream_;
+    std::vector<JSONBlock> blocks_;
+    std::string buffer_;
+    std::string::iterator buffer_it_;
+
+    inline void ParseUserEntry(JSONEntry<std::string>& entry);
+
+    inline void ParseQuestionEntry(JSONEntry<std::string>& entry);
+
+    inline void ParseScoreEntry(JSONEntry<int>& entry);
+
+    inline void ParseBlock(JSONBlock& block);
+
+  public:
+    explicit FullreadParser(const char* json_path) : f_in_stream_(json_path) {
+        std::getline(f_in_stream_, buffer_);
+    }
+
+    void Parse();
+
+    std::vector<JSONBlock>& GetData();
+};
+
+#endif  // JSON_LIB_BUFFERED_PARSER_HXX
