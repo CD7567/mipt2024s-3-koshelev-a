5aa500ed7590b66e6b6d876c9870c54cc199908e
CD7567 <alexander.y.koshelev@gmail.com>
2024-04-04 06:40:31 +0300

    Add TypePair

---
+++ prj.utils/type_pair.hpp
@@ -0,0 +1,10 @@
+#ifndef MIPT2024S_3_KOSHELEV_A_TYPE_PAIR_HPP
+#define MIPT2024S_3_KOSHELEV_A_TYPE_PAIR_HPP
+
+template <typename first, typename second>
+struct TypePair {
+    using F = first;
+    using S = second;
+};
+
+#endif  // MIPT2024S_3_KOSHELEV_A_TYPE_PAIR_HPP
