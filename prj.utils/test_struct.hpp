3279d1c4cfa6cac7778e929cd4beed0d9d2bd2cf
CD7567 <alexander.y.koshelev@gmail.com>
2024-04-04 03:41:43 +0300

    Make TestStruct comparable

--- prj.utils/test_struct.hpp
+++ prj.utils/test_struct.hpp
@@ -7,6 +7,10 @@
 struct TestStruct {
     size_t number_ = 0;
     std::string string_ = "I am string";
+
+    bool operator==(const TestStruct& other) const {
+        return number_ == other.number_ && string_ == other.string_;
+    }
 };
 
 #endif  // MIPT2024S_3_KOSHELEV_A_TEST_STRUCT_HPP
