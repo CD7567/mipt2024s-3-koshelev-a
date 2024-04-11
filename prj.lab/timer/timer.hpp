bd017d912fd6e94e53d4b64c9111ddd60ce64364
CD7567 <alexander.y.koshelev@gmail.com>
2024-04-11 12:05:03 +0300

    Make timer rely on string literals

--- prj.lab/timer/timer.hpp
+++ prj.lab/timer/timer.hpp
@@ -4,17 +4,21 @@
 #include <chrono>
 #include <iostream>
 
-template <typename ChronoT>
+template <typename ChronoT = std::chrono::nanoseconds>
 class Timer {
   private:
+    const char* function_name;
     std::chrono::time_point<std::chrono::high_resolution_clock> start_time_;
 
   public:
-    Timer() : start_time_(std::chrono::high_resolution_clock::now()) {}
+    Timer(const char* calling_function)
+        : start_time_(std::chrono::high_resolution_clock::now()),
+          function_name(calling_function) {}
 
     ~Timer() {
         auto end_time = std::chrono::high_resolution_clock::now();
-        std::cout << std::chrono::duration_cast<ChronoT>(end_time - start_time_)
+        std::cout << function_name << " named at " << (void*)function_name << ": "
+                  << std::chrono::duration_cast<ChronoT>(end_time - start_time_)
                   << "\n";
     };
 };
