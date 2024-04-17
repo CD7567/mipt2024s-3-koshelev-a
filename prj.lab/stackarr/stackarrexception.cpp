2d7892e1742dbacfb897f42c590bdc1378e9ac24
CD7567 <alexander.y.koshelev@gmail.com>
2024-04-18 02:14:55 +0300

    Make non-template stacks as instantiation

--- prj.lab/stackarr/stackarrexception.cpp
+++
@@ -1,5 +0,0 @@
-#include "stackarrexception.hpp"
-
-StackArrException::StackArrException(const char* msg) : message_(msg) {}
-
-const char* StackArrException::what() const noexcept { return message_; }
--- prj.lab/stacklst/stacklstexception.cpp
+++
@@ -1,5 +0,0 @@
-#include "stacklstexception.hpp"
-
-StackLstException::StackLstException(const char* msg) : message_(msg) {}
-
-const char* StackLstException::what() const noexcept { return message_; }
