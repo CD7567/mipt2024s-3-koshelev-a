44a866a6607744784216c0b7c10008d5bebd3b3f
CD7567 <alexander.y.koshelev@gmail.com>
2024-05-23 11:11:57 +0300

    Fix directory structure

--- prj.cw/src/app.cpp
+++ prj.cw/src/app.cpp
@@ -8,7 +8,7 @@
 int main(int argc, const char** argv) {
     // Configure sinks
     auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    console_sink->set_level(spdlog::level::debug);
+    console_sink->set_level(spdlog::level::info);
 
     auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/log.log", 1024 * 1024 * 10, 3);
     file_sink->set_level(spdlog::level::debug);
