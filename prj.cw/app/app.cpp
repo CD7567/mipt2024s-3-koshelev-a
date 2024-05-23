21cce4cd7833fdfd167d94f51dfbffa904b1acb3
CD7567 <alexander.y.koshelev@gmail.com>
2024-05-23 10:54:09 +0300

    Add logger

--- prj.cw/app/app.cpp
+++ prj.cw/app/app.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
-int main(int argc, const char** argv) {          // NOLINT
-    std::cout << "Hello world!" << "\n";
+#include <filesystem>
+#include <opencv4/opencv2/opencv.hpp>
+
+int main(int argc, const char** argv) {
+    // Configure sinks
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::debug);
+
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/log.log", 1024 * 1024 * 10, 3);
+    file_sink->set_level(spdlog::level::debug);
+
+    // Configure logger
+    spdlog::logger logger("prj.cw", {console_sink, file_sink});
+    logger.set_level(spdlog::level::debug);
 }
