1a25b7b3c4d7e319814b39107c82a3e4ae1bcda2
CD7567 <alexander.y.koshelev@gmail.com>
2024-05-25 23:11:31 +0300

    Add test data generator

---
+++ prj.cw/src/gen.cpp
@@ -0,0 +1,26 @@
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include "config_manager.hpp"
+
+int main(int argc, const char** argv) {
+    // Setup sinks
+    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    console_sink->set_level(spdlog::level::debug);
+
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        "logs/gen.log", 1024 * 1024 * 10, 3);
+    file_sink->set_level(spdlog::level::debug);
+
+    // Setup logger
+    spdlog::logger logger("cw/gen", {console_sink, file_sink});
+    logger.set_level(spdlog::level::debug);
+    spdlog::set_default_logger(std::make_unique<spdlog::logger>(logger));
+
+    logger.info("===== [Starting new run] =====");
+
+    // Setup config manager
+    auto& config = ConfigManager::getInstance();
+    config.setConfig("config.toml");
+}
